65bd7c66642541c511ed70abd14f65ec6b0cf30f
Jadson Danilo <Jadson17danilo@gmail.com>
2024-12-12 19:23:24 +0000

    revisando

--- src/repositories/usuario-repository.js
+++ src/repositories/usuario-repository.js
@@ -1,29 +1,44 @@
-import { database } from "./database";
+import database from "./database.js"
 
 
+// Repositório de usuários para manipular os dados no banco de dados
+const usuarioRepository = {
 
-export const findAll = () => database.usuarios;
+    //Retorna todos os usuários no bano de dados.
+    //@return {array} Lista de usuários.
+    findAll: () => database.usuarios,
 
-export const findByCpf = (cpf) => {
-    database.usuarios.find(usuarios => usuarios.cpf === cpf)
-};
+    // Busca um usuário pelo CPF.
+    //@param {string} cpf - O CPF do usuário a ser buscado.
+    //@return {Object|undefined} O usuário encontrado ou undefined se não encontrado.
+    findByCpf: (cpf) => {
+        return database.usuarios.find(usuario => usuario.cpf === cpf)
+    },
 
-export const save = (usuarios) => {
-    database.usuarios.push(usuarios);
+    //Salvar um novo usuário no banco de dados.
+    //@param {Object} usuario - O usuário a ser salvo.
+    //@param {string} usuario.cpf - O CPF do usuário.
+    //@param {straing} usuario.nome - O nome do usuário.
+    //@param {string} usuario.email - o email do usuário.
+    save: (usuario) => {
+        const isUserExists = usuarioRepository.findByCpf(usuario.cpf)
+        if (isUserExists) {
+            Object.assign(isUserExists, usuario)
+        } else {
+            database.usuarios.push(usuario)
+        }
+    },
+    
+    //Remove um usuário do banco de dados pelo CPF.
+    //@param {string} cpf - O CPF do usuário a ser removido.
+    remove: (cpf) => {
+        const index = database.usuarios.findIndex(usuario => usuario.cpf === cpf)
+        if (index !== -1) {
+            database.usuarios.splice(index, 1)
+        } else {
+            throw new Error(`Usuário com CPF ${cpf} não encontrado`)
+        }
+    }
 }
 
-export const update = (cpf, nome, fone, email) => {
-    const usuario = findBy(Cpf);
-    usuario.nome = nome;
-    usuario.cpf = cpf
-    usuario.fone = fone;
-    usuario.email = email;
-};
-
-export const remove = (cpf) => {
-    const index = database.usuarios.findIndex(usuario = usuario.cpf === cpf);
-    database.usuarios.splice(index, 1);
-}
-
-
-
+export default usuarioController;
